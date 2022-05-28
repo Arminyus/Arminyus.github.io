4f854600fc04a2eb80d66ec10314952214e375e8
Arminyus <102928959+Arminyus@users.noreply.github.com>
2022-05-27 21:26:48 -0400

    1

--- time.js
+++ time.js
@@ -1,4 +1,6 @@
 var today = new Date();
 var timeNow = today.getTime();
 
-document.write("Time of last refresh: " + timeNow);
+for (var i=0; i<50; i++){
+    document.write("Time of last refresh: " + timeNow);
+}
